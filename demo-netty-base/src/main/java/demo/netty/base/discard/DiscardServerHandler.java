33a944de8b5195db27194585163c75198e97c799
tshch1989 <tshch0115>
2020-06-23 18:10:10 +0800

    'init'

---
+++ demo-netty-base/src/main/java/demo/netty/base/discard/DiscardServerHandler.java
@@ -0,0 +1,20 @@
+package demo.netty.base.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println(buf.readableBytes());
+        buf.release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
